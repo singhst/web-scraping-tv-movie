f6f9728478606c6f36f8cb0cf362af41fee89f02
singhst <workworkwork1920@gmail.com>
2021-06-02 18:01:54 +0800

    update

--- test/selenium-movie-scrape-after-click-button.py
+++ test/selenium-movie-scrape-after-click-button.py
@@ -12,12 +12,13 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def writeToFile(content: Iterable[str], 
+def writeToFile(content: Iterable[str],
                 file_name: str = "extracted",
                 file_type: str = "txt",
                 file_path: str = "test"):
 
-    print(f'> writing "{file_name}.{file_type}" to path "/{file_path}/"', end = '')
+    print(
+        f'> writing "{file_name}.{file_type}" to path "/{file_path}/"', end='')
 
     if isinstance(content, list):
         if not isinstance(content[0], str):
@@ -28,9 +29,9 @@ def writeToFile(content: Iterable[str],
     # Program to show various ways to read and
     # write data in a file.
     file1 = open(f"{file_path}/{file_name}.{file_type}", "w")
-    
+
     file1.writelines(content)
-    file1.close() #to change file access modes
+    file1.close()  # to change file access modes
 
     print('\t ==> DONE!')
 
@@ -41,25 +42,45 @@ options.add_argument("--test-type")
 # options.binary_location = "/usr/bin/chromium"
 driver = webdriver.Chrome(chrome_options=options)
 
+start_time = time.time()
+
 # driver.get('https://reelgood.com/uk/movie/3-idiots-2009')
 driver.get('https://reelgood.com/movie/lady-bird-2017')
 
+# # click "Stream Movie" button
+# moreButton = driver.find_element_by_xpath("//button[@class='css-hpmncj eyx6tna3']")
+# moreButton.click()
 
-# click "Stream Movie" button
-moreButton = driver.find_element_by_xpath("//button[@class='css-hpmncj eyx6tna3']")
-moreButton.click()
+# # Wait for a while
+# # wait = WebDriverWait(driver, 10)
+# # element = wait.until(EC.invisibility_of_element_located((By.XPATH, "//button[@class='css-1ks2bp0 e126mwsw1']")))
+# time.sleep(2)
 
+# # in pop-up window, click "Rent/Buy from xxx"
+# moreButton = driver.find_element_by_xpath("//button[@class='css-rshlkp e126mwsw1']")
+# moreButton.click()
 
-# wait = WebDriverWait(driver, 10)
-# element = wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, "css-rshlkp e126mwsw1")))
+jsScript = driver.find_element_by_xpath("//script[@type='text/javascript']")
+print(str(jsScript.get_attribute("outerHTML")))
+# script_text = driver.find_element_by_xpath("//script[contains(.,'_rg.update(')]").text
+# print(script_text) 
 
-time.sleep(4)
+# # Wait for a while
+# time.sleep(2)
 
-# in pop-up window, click "Rent/Buy from xxx"
-moreButton = driver.find_element_by_xpath("//button[@class='css-rshlkp e126mwsw1']")
-moreButton.click()
-moreButton.click()
-
-
+# Scrap the web page containing (1) description text, (2) Stream links (3) Rent/But links
 html_source = driver.page_source
+
+bsObj = BeautifulSoup(html_source, 'html.parser')
+bsElems = bsObj.find_all('script', type='text/javascript')
+
+result = []
+for x in bsElems:
+    # print(x.string)
+    result.append(x.string)
+
+
+end_time = time.time()
+writeToFile(result, "extracted_json", 'json')
 writeToFile(html_source, "extracted_after_btn_click", 'html')
+print("time =", end_time - start_time)
