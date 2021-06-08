0c42296a790b3fd939c687b790305acfca21d1aa
singhst <workworkwork1920@gmail.com>
2021-06-08 14:10:29 +0800

    can scrape movie tv show as json

--- test/2-scrape-each-title-link-description.py
+++ test/2-scrape-each-title-link-description.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import json
+import re
 
 import os, sys
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -73,7 +74,9 @@ class webScrapeEachTitleDetail():
         self.rg_id = ''
         self.title = ''
         self.description = ''
+        self._title_links_dict_list = Iterable[dict]
         self.title_links = Iterable[str]
+        self._cast_crew_dict_list = Iterable[dict]
         self.cast_crew = Iterable[str]
 
 
@@ -87,7 +90,7 @@ class webScrapeEachTitleDetail():
         
         movie_or_show: `str`, only accept `movie` or `show` because of the Reelgood url pattern.
 
-        title: `str`, title of the movie/TV show
+        title: `str`, title of the movie/TV show, e.g. `Godzilla vs. Kong`
 
         year: `str`, publishing year
         """
@@ -160,6 +163,9 @@ class webScrapeEachTitleDetail():
 
 
     def extractJsTagJson(self) -> dict:
+        """Because we found that the structural info of a movie/tv show is stored inside a JSON 
+        under HTML tag <script type="text/javascript">
+        """
         # structured json data is in the HTML tag <script type="text/javascript">
         bs_elems = self.html_page_soup_object.find_all('script', type='text/javascript')
         bs_single_elem = self.cleanMetaData(bs_elems)
@@ -181,11 +187,17 @@ class webScrapeEachTitleDetail():
 
         temp_str = temp_str.replace("_rg.update(", "")
         temp_str = temp_str.replace("})", "}")
-        temp_str = temp_str.replace("\\u002F", "/")
+        # temp_str = temp_str.replace("\\u002F", "/")
+        temp_str = self.unicodeToChar(temp_str)
         temp_str = temp_str.replace('undefined', '"undefined"')
         # print("> type(temp_str)=", type(temp_str))
 
         return temp_str
+
+
+    def unicodeToChar(self, input_str: str) -> str:
+        # find()
+        return input_str
 
 
     def strToDict(self, string: str) -> dict:
@@ -197,27 +209,56 @@ class webScrapeEachTitleDetail():
 
     def extractTitleDetail(self):
         """
-        
+        self.title_detail_dict = {}
+        self.rg_id = ''
+        self.title = ''
+        self.description = ''
+        self.title_links = Iterable[str]
+        self.cast_crew = Iterable[str]
         """
+        #First item in dict is the info about the movie/tv show 
         _a_dict = self.meta_data.values()
         _a_dict = iter(_a_dict)
         self.title_detail_dict = next(_a_dict)
 
         # print("\t> self.title_detail_dict =", self.title_detail_dict)
         self.rg_id = self.title_detail_dict['rg_id']
+        self.title = self.title_detail_dict['title']
         self.description = self.title_detail_dict['overview']
-        self.title_links = self.title_detail_dict['availability']
-        self.cast_crew = self.extractCastCrew()
+        self.title_links, self._title_links_dict_list = self.extractLinks()
+        self.cast_crew, self._cast_crew_dict_list = self.extractCastCrew()
 
 
-    def extractCastCrew(self) -> dict:
+    def extractLinks(self) -> list:
+        """
+        Return 
+        ------
+        links: `dict`, {"links": ["www.xxx", "www.yyy", "www.zzz"]}
+
+        links_dict_list: `Iterable[dict]`
+        """
+        links_dict_list = self.title_detail_dict['availability'] #it is a list of dict
+        links = {"links": [link_dict['source_data']['web_link'] for link_dict in links_dict_list]}
+        return links, links_dict_list
+
+
+    def extractCastCrew(self) -> list:
         """NOT FINISHED.
+        Missing the roles of each cast & crew. need to match by persion ID.
+
+        Return 
+        ------
+        cast_crew: `dict`
+        
+        cast_crew_dict_list: `Iterable[dict]`
         """
         search_key = 'person'
         # Using items() + list comprehension
         # Substring Key match in dictionary
-        res = [val for key, val in self.meta_data.items() if search_key in key]
-        return res
+        cast_crew_dict_list = [val for key, val in self.meta_data.items() if search_key in key]
+        cast_crew = cast_crew_dict_list
+
+        return cast_crew, cast_crew_dict_list
 
 
     def getHtmlPage(self) -> BeautifulSoup.new_tag:
@@ -239,6 +280,7 @@ class webScrapeEachTitleDetail():
 
 
     def getDescription(self) -> str:
+        """No use"""
         p = self.html_page_soup_object.find('p', itemprop="description")
         for x in p:
             return str(x)
@@ -255,6 +297,7 @@ class webScrapeEachTitleDetail():
 
 
     def getLink(self) -> list:
+        """No use"""
         ls = []
         for link in self.html_page_soup_object.find_all('a', itemprop="url"):
             ls.append(str(link.get('href'))+"\n")
@@ -262,41 +305,13 @@ class webScrapeEachTitleDetail():
         return ls
 
 
-    def getLinkFromMetaData(self) -> list:
-        return
-
-
-# def writeToFile(content: Iterable[str], 
-#                 file_name: str = "extracted",
-#                 file_type: str = "txt",
-#                 file_path: str = "test"):
-
-#     print(f'> writing "{file_name}.{file_type}" to path "/{file_path}/"', end = '')
-
-#     if isinstance(content, list):
-#         if not isinstance(content[0], str):
-#             content = [str(c) for c in content]
-#     elif not isinstance(content, str):
-#         content = str(content)
-
-#     # Program to show various ways to read and
-#     # write data in a file.
-#     file1 = open(f"{file_path}/{file_name}.{file_type}", "w")
-    
-#     file1.writelines(content)
-#     file1.close() #to change file access modes
-
-#     print('\t ==> DONE!')
-
-
 def main():
-
+    # Create folders to save scraped data
     current_path = os.getcwd()
-    folder_name = 'temp_save'
     path = os.path.join(current_path, 'test')
 
-    # Get TV show or Movie data
-    export_path = folderCreate(path, folder_name)
+    temp_save_path = folderCreate(path, 'temp_save')
+    demo_save_path = folderCreate(path, 'demo_save')
 
     # vgm_url = 'https://www.vgmusic.com/music/console/nintendo/nes/'
     # vgm_url = 'https://reelgood.com/show/the-good-doctor-2017'
@@ -305,42 +320,54 @@ def main():
     # vgm_url = 'https://reelgood.com/movie/hellboy-2019'
     # vgm_url = 'https://reelgood.com/movie/the-intouchables-2011'
     # vgm_url = 'https://reelgood.com/movie/3-idiots-2009'
-    vgm_url = 'https://reelgood.com/movie/lady-bird-2017'
+    # vgm_url = 'https://reelgood.com/movie/lady-bird-2017'
     
+    """
+    ('Godzilla vs. Kong', '2020')
+    ('Joker', '2019') 
+    ('Lady Bird', '2017') 
+    ('3 Idiots', '2009') 
+    """
     url_domain = 'https://reelgood.com'
     movie_or_show = 'movie'
-    title = 'Lady Bird' #'3 Idiots'
-    year = '2017'       #2009
+    title, year = ('Lady Bird', '2017') 
 
+    # remove title's symbols ==> keep only letter, number and space char
+    title_no_symbol = re.sub(r'[^a-zA-Z0-9 ]+', '', title)
+    title_no_symbol = title_no_symbol.replace(' ', '_')
+
+    # Get TV show or Movie data
     scraper = webScrapeEachTitleDetail()
     scraper.setUrl(url_domain, movie_or_show, title, year)
     
 
-    # scraper.scrapeHtmlPageBeatifulSoup()
-    # soup = scraper.getHtmlPage()    #scraper.html_page_soup_object
-    # writeToFile(soup, "extracted_html_soup", "html")
-    
-
+    #Get HTML code
     scraper.scrapeHtmlPageSelenium()
     soup = scraper.html_page_soup_object    #scraper.getHtmlPage()
     writeToFile(soup,
-                f"extracted_{title}_{year}_0_html_selenium", 
+                f"extracted_{title_no_symbol}_{year}_0_html_selenium", 
                 "html", 
-                export_path)
+                temp_save_path)
 
+
+    #Get .js json from <script type='text/javascript'> tag 
+    #This json contains all information about the movie/tv show
     meta_data = scraper.getMetaData()
     str_meta_data = json.dumps(meta_data)
     writeToFile(str_meta_data,
-                f"extracted_{title}_{year}_1_meta", 
+                f"extracted_{title_no_symbol}_{year}_1_meta", 
                 "json", 
-                export_path)
+                temp_save_path)
 
+
+    #Extract (1) description (2) links (3) cast & crew etc.
     scraper.extractTitleDetail()
     title_detail_dict = scraper.title_detail_dict
     writeToFile(json.dumps(title_detail_dict),
-                f"extracted_{title}_{year}_2_{movie_or_show}_detail", 
+                f"extracted_{title_no_symbol}_{year}_2_{movie_or_show}_detail", 
                 "json", 
-                export_path)
+                temp_save_path)
+
 
     movie_id = scraper.rg_id
     movie_title = scraper.title
@@ -348,23 +375,36 @@ def main():
     links = scraper.title_links
     cast_crew = scraper.cast_crew
 
+    _title_links_dict_list = scraper._title_links_dict_list
+    _cast_crew_dict_list = scraper._cast_crew_dict_list
+
     writeToFile(description,
-                f"extracted_{title}_{year}_3_description",
-                file_path=export_path)
+                f"extracted_{title_no_symbol}_{year}_description",
+                file_path=demo_save_path)
     writeToFile(json.dumps(links),
-                f"extracted_{title}_{year}_4_links",
+                f"extracted_{title_no_symbol}_{year}_links",
                 "json",
-                export_path)
+                demo_save_path)
     writeToFile(json.dumps(cast_crew),
-                f"extracted_{title}_{year}_4_links",
+                f"extracted_{title_no_symbol}_{year}_cast_crew",
                 "json",
-                export_path)
+                demo_save_path)
+
+
+    writeToFile(json.dumps(_title_links_dict_list),
+                f"extracted_{title_no_symbol}_{year}_4_links_dict_list",
+                "json",
+                temp_save_path)
+    writeToFile(json.dumps(_cast_crew_dict_list),
+                f"extracted_{title_no_symbol}_{year}_5_cast_crew_dict_list",
+                "json",
+                temp_save_path)
 
 
     print("1) movie_id:", movie_id)
     print("2) movie_title:", movie_title)
-    print("3) description[:20]:", description[:20])
-    print(f"4) links[0]: {type(links)}, {links[0]}")
+    print("3) description:", description)
+    print(f"4) links: {type(links)}, {links}")
     print("5) cast_crew[0]:", cast_crew[0])
 
 
