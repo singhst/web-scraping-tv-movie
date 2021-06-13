621438761f348d7696834258fb5856a41bf8a7c2
singhst <singhst@users.noreply.github.com>
2021-06-13 17:50:21 +0800

    can create table

--- helper_db/databaseMysql/createTable.py
+++ helper_db/databaseMysql/createTable.py
@@ -1,9 +1,29 @@
+from mysql.connector import Error as mysqlError
 
-def createTable(db_connection):
+
+def createTable(db_connection, table_name: str):
     """
     """
+    print(f'> Creating table `{table_name}` in `{db_connection.database}` database... ', end='')
 
     # creating database_cursor to perform SQL operation
     db_cursor = db_connection.cursor()
+    
+    # sql query
+    sql_query = f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+            id INT NOT NULL, 
+            Title VARCHAR(255), 
+            Year INT, 
+            Rating INT, 
+            IMDBScore INT, 
+            ReelgoodRatingScore INT
+        );
+    '''
 
-    sql_query = ''
+    try:
+        db_cursor.execute(sql_query)
+        print('==> Done!')
+
+    except(Exception, mysqlError) as error:
+        print(f'> Error = `{error}`')
--- test-import-module.py
+++ test-import-module.py
@@ -1,6 +1,9 @@
 from helper.translateToUrlPath import translateToUrlPath
 from helper_db.databaseMysql.setupDatabase import setupDatabase
+from helper_db.databaseMysql.createTable import createTable
 
 db_name = movies_or_tv = 'movies' # 'tv' #
 db = setupDatabase(db_name)
 db_connection = db.getConnection()
+
+createTable(db_connection, 'movie')
